362c1bc070237f03a2ec3dcd5659510789f4bb33
testetson22 <testetson@gmail.com>
2025-03-14 17:08:05 -0400

    works within reasonable bounds

--- sandpiles/__main__.py
+++ sandpiles/__main__.py
@@ -1,17 +1,17 @@
 import sys
 from .sandpiles import *
 
-#TODO support proper flags for args
+#TODO support proper flags, type handling, etc for args
 def main():
     print('in main')
     args = sys.argv[1:]
-    x = args[0]
-    y = args[1]
-    z = args[2]
+    x = int(args[0])
+    y = int(args[1])
+    z = int(args[2])
     print('')
 
     sand_pile = SandPile(x, y, z)
-    sand_pile.apply_gravity()
+    print(sand_pile.apply_gravity())
 
 if __name__ == '__main__':
     main()
--- sandpiles/sandpiles.py
+++ sandpiles/sandpiles.py
@@ -4,9 +4,11 @@ import numpy as np
 class SandPile():
     def __init__(self, x, y, z):
         self.sandpile = np.zeros((x, y), dtype=np.uint32)
-        #TODO calculate centerpoint
-        self.sandpile[2, 2] = z
-        
+        rows, cols = self.sandpile.shape
+        row = rows // 2
+        col = cols // 2        
+        self.sandpile[row, col] = z
+
     def apply_gravity(self):
         sandpile = self.sandpile
         indicies = list(zip(*np.where(sandpile >= 4)))
@@ -23,6 +25,5 @@ class SandPile():
                     sandpile[x, y-1] += 1
                     sandpile[x, y+1] += 1
                     sandpile[x+1, y] += 1
-            self.apply_gravity(sandpile)
-            
+            self.apply_gravity()            
         return sandpile
