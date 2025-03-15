b6d06e95a78056760373decd85933918d971c710
testetson22 <testetson@gmail.com>
2025-03-14 22:49:03 -0400

    docs and TODOs

--- sandpiles/sandpiles.py
+++ sandpiles/sandpiles.py
@@ -3,6 +3,16 @@ import numpy as np
 
 class SandPile():
     def __init__(self, x, y, z):
+        """Initializes a SandPile instance that consists of a 2D Array and initial pile of sand reresented on the z axis
+
+        Args:
+            x (int): array dimension
+            y (int): array dimension
+            z (int): specifies pile height
+        """
+        #TODO decrement should be derived from the max height and number of distributions.
+        self.max_height = 4
+        self.decrement = self.max_height
         self.sandpile = np.zeros((x, y), dtype=np.uint32)
         rows, cols = self.sandpile.shape
         row = rows // 2
@@ -10,8 +20,15 @@ class SandPile():
         self.sandpile[row, col] = z
 
     def apply_gravity(self):
+        """Applying gravity will cause qny pile with a height greater than 4 to collapse and be redistributed to adjacent positions in the array
+
+        Returns:
+            SandPile: a sandpile to apply gravity constraints to. 
+        """
         sandpile = self.sandpile
-        indicies = list(zip(*np.where(sandpile >= 4)))
+        max_height = self.max_height
+        decrement = self.decrement
+        indicies = list(zip(*np.where(sandpile >= max_height)))
         if not indicies:
             sandpile
         else:
@@ -20,9 +37,9 @@ class SandPile():
                 x = index[0]
                 y = index[1]
                 indicies_to_incr = [(x-1, y),(x, y-1),(x, y+1),(x+1, y)]
-                while sandpile[x, y] >= 4:
-                    sandpile[x, y] -= 4
+                while sandpile[x, y] >= max_height:
+                    sandpile[x, y] -= decrement
                     for x, y in indicies_to_incr:
-                        sandpile[x, y] += 1
+                        sandpile[x, y] += decrement
             self.apply_gravity()            
         return sandpile
